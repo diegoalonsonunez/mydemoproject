b2f0cea8cac30321c7a3d6d0b5b42484289a1be9
diegoalonsonunez <diego96danh@gmail.com>
2018-07-18 13:12:23 -0400

    adding new class B and updating class A

--- src/test1/ClassA.java
+++ src/test1/ClassA.java
@@ -1,5 +1,12 @@
 package test1;
 
 public class ClassA {
+	public int a;
+	
+	public ClassA (int a) {
+		this.a = a;
+		
+		
+	}
 
 }
---
+++ src/test1/ClassB.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		System.out.println("this is class B");
+	}
+	
+	
+}
